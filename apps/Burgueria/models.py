0fba74f02dbdd70693668b75c64bff2a115cdacd
Marcos <devmarcos7@gmail.com>
2025-03-07 23:15:47 -0300

    criando model

--- apps/Burgueria/models.py
+++ apps/Burgueria/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Sanduiche(models.Model):
+    """
+        Modelo que representa um sanduiche no banco de dados   
+    """    
     nome = models.CharField(max_length=100),
     
